777b201a3de352df5b049b77ca75b0887c22c1a8
JINMUN PARK <Jinmun.Park@ibm.com>
2021-04-23 00:01:58 +0800

    Normalization

--- src/model/airpassengers.py
+++ src/model/airpassengers.py
@@ -46,3 +46,47 @@ seq_size = 5
 
 trainX, trainY = to_sequences(train, seq_size)
 testX, testY = to_sequences(test, seq_size)
+
+print("Shape of training set : {}".format(trainX.shape))
+print("Shape of test set : {}".format(testX.shape))
+
+
+# Modelling (Large Dens can cause overfitting)
+
+model = Sequential()
+model.add(Dense(64, input_dim=seq_size, activation='relu')) #12
+#model.add(Dense(32, activation='relu')) #8
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam', metrics = ['acc'])
+print(model.summary())
+
+# Fit & Prediction
+model.fit(trainX, trainY, validation_data=(testX, testY), verbose=2, epochs=100)
+
+trainpredict = model.predict(trainX)
+testpredict = model.predict(testX)
+
+TrainPredict = transform.inverse_transform(trainpredict)
+TrainY_inverse = transform.inverse_transform([trainY])
+TestPredict = transform.inverse_transform(testpredict)
+TestY_inverse = transform.inverse_transform([testY])
+
+# RMSE
+trainscore = math.sqrt(mean_squared_error(TrainY_inverse[0], TrainPredict[:,0]))
+print('Train Score : %.2f RMSE' % (trainscore))
+testscore = math.sqrt(mean_squared_error(TestY_inverse[0], TestPredict[:,0]))
+print('Test Score : %.2f RMSE' % (testscore))
+
+# Plot
+trainpredictplot = np.empty_like(df_ar)
+trainpredictplot[:, :] = np.nan
+trainpredictplot[seq_size:len(TrainPredict)+seq_size, :] = TrainPredict
+
+testpredictplot = np.empty_like(df_ar)
+testpredictplot[:, :] = np.nan
+testpredictplot[len(TrainPredict)+(seq_size*2)+1:len(df_ar)-1,:] = TestPredict
+
+plt.plot(transform.inverse_transform(df_ar))
+plt.plot(trainpredictplot)
+plt.plot(testpredictplot)
+plt.show()
