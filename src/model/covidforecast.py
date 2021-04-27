45e4ac0c8616df2daafca146ca433f653c850216
JINMUN PARK <Jinmun.Park@ibm.com>
2021-04-27 18:36:32 +0800

    update_2

--- src/model/covidforecast.py
+++ src/model/covidforecast.py
@@ -22,6 +22,10 @@ https://www.youtube.com/watch?v=97bZKO6cJfg&t=870s&ab_channel=DigitalSreeni
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler #Step 3
+from keras.preprocessing.sequence import TimeseriesGenerator #Step 4
+from keras.models import Sequential #Step 5
+from keras.layers import Dense, LSTM, Dropout, Activation
 
 pd.set_option('display.max_rows', None)
 
@@ -30,7 +34,7 @@ print(df_confirmed.columns)
 print(df_confirmed['Country/Region'].unique)
 country = "US"
 
-#Total COVID confirmed cases
+## Step 1 : Data Sreucture
 
 df_confirmed_country = df_confirmed[df_confirmed["Country/Region"] == country]
 df_confirmed_country = pd.DataFrame(df_confirmed_country[df_confirmed_country.columns[4:]].sum(),columns=["confirmed"])
@@ -38,69 +42,62 @@ df_confirmed_country.index = pd.to_datetime(df_confirmed_country.index,format='%
 
 df_confirmed_country.plot(figsize=(10,5),title="COVID confirmed cases")
 
+df_confirmed_country.head(10)
 df_confirmed_country.tail(10)
-
+print("First 10 days", df_confirmed_country.head(10))
+print("Last 10 days", df_confirmed_country.tail(10))
 print("Total days in the dataset", len(df_confirmed_country))
 
-#Use data until 14 days before as training
+## Step 2 : Training & Test (14 days interval)
 x = len(df_confirmed_country)-14
 
-train=df_confirmed_country.iloc[:x]
+train = df_confirmed_country.iloc[:x]
 test = df_confirmed_country.iloc[x:]
 
-##scale or normalize data as the data is too skewed
-from sklearn.preprocessing import MinMaxScaler
+## Step 3 : Normalization
 scaler = MinMaxScaler()
 scaler.fit(train)
 
 train_scaled = scaler.transform(train)
 test_scaled = scaler.transform(test)
 
-## Use TimeSeriestrain_generator to generate data in sequences.
-#Alternatively we can create our own sequences.
-from keras.preprocessing.sequence import TimeseriesGenerator
-
-#Sequence size has an impact on prediction, especially since COVID is unpredictable!
+## Step 4 : TS conversion
 seq_size = 7  ## number of steps (lookback)
 n_features = 1 ## number of features. This dataset is univariate so it is 1
+
 train_generator = TimeseriesGenerator(train_scaled, train_scaled, length = seq_size, batch_size=1)
-print("Total number of samples in the original training data = ", len(train)) # 271
-print("Total number of samples in the generated data = ", len(train_generator)) # 264 with seq_size=7
+print("Total number of samples in the original training data = ", len(train))
+print("Total number of samples in the generated data = ", len(train_generator))
 
-#Check data shape from generator
-x,y = train_generator[10]  #Check train_generator
-#Takes 7 days as x and 8th day as y (for seq_size=7)
-
-#Also generate test data
 test_generator = TimeseriesGenerator(test_scaled, test_scaled, length=seq_size, batch_size=1)
 print("Total number of samples in the original training data = ", len(test)) # 14 as we're using last 14 days for test
 print("Total number of samples in the generated data = ", len(test_generator)) # 7
-#Check data shape from generator
-x,y = test_generator[0]
 
-from keras.models import Sequential
-from keras.layers import Dense, LSTM, Dropout, Activation
-
-#Define Model
+## Step 5 : Modelling
 model = Sequential()
-model.add(LSTM(150, activation='relu', return_sequences=True, input_shape=(seq_size, n_features)))
+model.add(LSTM(128, activation='relu', return_sequences=True, input_shape=(seq_size, n_features)))
 model.add(LSTM(64, activation='relu'))
-model.add(Dense(64))
+model.add(Dense(32))
 model.add(Dense(1))
 model.compile(optimizer='adam', loss='mean_squared_error')
 
 model.summary()
-print('Train...')
-##########################
 
-history = model.fit_generator(train_generator,
+## Step 6 : Fitting
+'''
+fit_generator will be depreciated, check this out ;
+https://stackoverflow.com/questions/60585948/convert-model-fit-generator-to-model-fit
+'''
+
+history = model.fit(train_generator,
                               validation_data=test_generator,
                               epochs=50, steps_per_epoch=10)
 
 
-#plot the training and validation accuracy and loss at each epoch
 loss = history.history['loss']
 val_loss = history.history['val_loss']
+print('This is training loss', len(loss))
+print('This is validation loss', len(val_loss))
 epochs = range(1, len(loss) + 1)
 plt.plot(epochs, loss, 'y', label='Training loss')
 plt.plot(epochs, val_loss, 'r', label='Validation loss')
@@ -110,9 +107,27 @@ plt.ylabel('Loss')
 plt.legend()
 plt.show()
 
+"""
+1) Underfitting
 
-#forecast
-prediction = [] #Empty list to populate later with predictions
+This is the only case where loss > validation_loss, but only slightly, if loss is far higher than validation_loss, please post your code and data so that we can have a look at
+
+2) Overfitting
+
+loss << validation_loss
+
+This means that your model is fitting very nicely the training data but not at all the validation data, in other words it's not generalizing correctly to unseen data
+
+3) Perfect fitting
+
+loss == validation_loss
+
+If both values end up to be roughly the same and also if the values are converging (plot the loss over time) then chances are very high that you are doing it right
+"""
+
+## Step 7 : Forecast
+
+prediction = []
 
 current_batch = train_scaled[-seq_size:] #Final data points in train
 current_batch = current_batch.reshape(1, seq_size, n_features) #Reshape
@@ -122,7 +137,7 @@ future = 7 #Days
 for i in range(len(test) + future):
     current_pred = model.predict(current_batch)[0]
     prediction.append(current_pred)
-    current_batch = np.append(current_batch[:,1:,:],[[current_pred]],axis=1)
+    current_batch = np.append(current_batch[:,1:,:],[[current_pred]],axis=1) #np.append
 
 ### Inverse transform to before scaling so we get actual numbers
 rescaled_prediction = scaler.inverse_transform(prediction)
